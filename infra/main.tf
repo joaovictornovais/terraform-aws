60f87bdb28cd49130545009c69910718df8647c3
João Victor <joao.dipaay@gmail.com>
2024-06-20 22:33:07 -0300

    feat: creating keypair in AWS with terraform

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_key_pair" "keypair" {
+  key_name = "terraform-keypair"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
 resource "aws_security_group" "securitygroup" {
   name = "securitygroup"
   description = "Permitir acesso HTTP e acesso a Internet"
@@ -9,6 +14,13 @@ resource "aws_security_group" "securitygroup" {
   ingress {
     from_port = 80
     to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -25,5 +37,6 @@ resource "aws_instance" "servidor" {
   ami = "ami-08a0d1e16fc3f61ea"
   instance_type = "t2.nano"
   user_data = file("user_data.sh")
+  key_name = aws_key_pair.keypair.key_name
   vpc_security_group_ids = [aws_security_group.securitygroup.id]
 }
